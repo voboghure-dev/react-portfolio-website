31eae5a23a137aad1e1d772a70c88c4f91539770
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-12 12:32:19 +0600

    Complete 5th lecture
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typical from 'react-typical';
 
 function Profile(props) {
   return (
@@ -6,20 +7,58 @@ function Profile(props) {
       <div className='profile-parent'>
         <div className='profile-details'>
           <div className='colz'>
-						<a href='#'>
-							<i className='fa fa-facebook-square'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-google-plus-square'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-instagram'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-youtube-square'></i>
-						</a>
-						<a href='#'>
-							<i className='fa fa-twitter'></i>
+            <a href='https://github.com/voboghure-dev'>
+              <i className='fab fa-github-square share'></i>
+            </a>
+            <a href='https://twitter.com/voboghure_dev'>
+              <i className='fab fa-twitter-square'></i>
+            </a>
+            <a href='https://www.linkedin.com/in/tapankumerdas'>
+              <i className='fab fa-linkedin'></i>
+            </a>
+            <a href='https://www.instagram.com/voboghure.dev/'>
+              <i className='fab fa-instagram-square'></i>
+            </a>
+          </div>
+          <div className='profile-details-name'>
+            <span className='primary-text'>
+              {' '}
+              Hello, I'm <span className='highlighted-text'>Tapan</span>
+            </span>
+          </div>
+          <div className='profile-details-role'>
+            <span className='primary-text'>
+              {' '}
+              <h1>
+                {' '}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    'Enthusiastic Dev 😍',
+                    1000,
+                    'Full Stack Developer 👨🏻‍💻',
+                    1000,
+                    'LAMP Stack Dev',
+                    1000,
+                    'React Dev',
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className='profile-role-tagline'>
+                I am building infrastructure for creative web application.
+                Focused on great performance and deliver the best results as
+                daily basis.
+              </span>
+            </span>
+          </div>
+					<div className='profile-options'>
+						<button className='btn primary-btn'>
+							{" "}
+							Hire Me{" "}
+						</button>
+						<a href='Tapan.pdf' download='Tapan.pdf'>
+							<button className='btn highlighted-btn'>Get Resume</button>
 						</a>
 					</div>
         </div>
